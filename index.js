287c5566bf5f7f73c6498b39e307f83536f1b943
sm-learning <smlearning022@gmail.com>
2023-03-20 07:58:00 +0530

    added new context

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send(`Welcome to Expressapp`);
 });
+app.get("/app", (req, res) => {
+  res.send(`Welcome to New Expressapp`);
+});
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
